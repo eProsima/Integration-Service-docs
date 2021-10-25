e3be3317feac34a90160280e5e47abe8a18858d9
Miguel Barro <miguelbarro@eprosima.com>
2021-10-25 12:26:56 +0200

    [12458] Ancillary files
    Signed-off-by: Miguel Barro <miguelbarro@eprosima.com>

--- docs/conf.py
+++ docs/conf.py
@@ -620,7 +620,7 @@ html_context = {
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
 #
-# html_extra_path = []
+html_extra_path = ['aux_files']
 
 # If not None, a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.
